7e75c00365dd19efa60aa0ebb6a81051762f2770
CrypticEnigma <30286773+CrypticEngima@users.noreply.github.com>
2020-08-09 22:54:54 +0100

    Added a channel model

---
+++ Modals/Channel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StreamMultiChat.Blazor.Modals
+{
+	public class Channel
+	{
+		public string Name { get; }
+		public List<Macro> Macros { get; } = new List<Macro>();
+
+		public Channel(string channelName)
+		{
+			Name = channelName;
+		}
+
+		public	IEnumerable<(string channel,string message)> GetMessageToSend(string message)
+		{
+			foreach (var macro in GetMacrosToRun(message))
+			{
+				yield return (Name, macro.Response);
+			}
+		}
+
+		private IEnumerable<Macro> GetMacrosToRun(string message)
+		{
+			return Macros.Where(m => m.IsEnabled && m.Command == message);
+		}
+
+		public void AddMacro(Macro macro)
+		{
+			if ((macro.Channel == Name || macro.Channel == null) && (!Macros.Contains(macro)))
+			{
+				Macros.Add(macro);
+			}
+		}
+
+		public void RemoveMacro(Macro macro)
+		{
+			Macros.Remove(macro);
+		}
+	}
+}
