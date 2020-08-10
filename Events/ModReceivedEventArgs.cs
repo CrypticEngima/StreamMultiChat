74169a7e76d6b4aca97b81fd9323b14f2c5427b3
CrypticEnigma <30286773+CrypticEngima@users.noreply.github.com>
2020-08-11 00:59:22 +0100

    Started Adding Moderator Controls

---
+++ Events/ModReceivedEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using StreamMultiChat.Blazor.Modals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StreamMultiChat.Blazor.Events
+{
+	public class ModReceivedEventArgs
+	{
+		public string Channel { get; }
+		public IList<string> Mods { get; }
+
+		public ModReceivedEventArgs(string channel, IList<string> mods)
+		{
+			Channel = channel;
+			Mods = mods;
+		}
+	}
+}
